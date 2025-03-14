37bc9e474e5448efe44603000c3702865b27e539
Danilo <danilo.koljensic.fit@gmail.com>
2025-03-14 15:19:46 +0100

    Filtering games by genre

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 
 
 interface FetchResponse<T> {
@@ -8,7 +8,7 @@ interface FetchResponse<T> {
     results: T[]
 }
 
-const useData = <T>(endopoint: string) => {
+const useData = <T>(endopoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
 
 const [data, setData] = useState<T[]>([]);
     const [error, setError] = useState("");
@@ -19,7 +19,7 @@ const [data, setData] = useState<T[]>([]);
 
         setLoading(true)
         apiClient
-          .get<FetchResponse<T>>(endopoint, { signal: controller.signal})
+          .get<FetchResponse<T>>(endopoint, { signal: controller.signal, ...requestConfig})
           .then((res) => {
             setData(res.data.results)
             setLoading(false)
@@ -32,7 +32,7 @@ const [data, setData] = useState<T[]>([]);
           
 
           return () => controller.abort()
-      }, []);
+      }, deps ? [...deps] : []);
     
       return {data, error, isLoading}
 
